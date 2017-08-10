79087bfadc52405429de4e147310560b7500a5ce
Lenon Tolfo <le.tolfo@yahoo.com.br>
2017-08-09 22:52:06 -0300

    criado a empresa activity

--- AndroidStudioProjects/AtmEmpresaConsultoria/app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
+++ AndroidStudioProjects/AtmEmpresaConsultoria/app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
@@ -3,12 +3,19 @@ package atmempresaconsultoria.cursoandroid.com.atmempresaconsultoria;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+    private Button botaoEmpresa;
+    private Button botaoContato;
+    private Button botaoServico;
+    private Button botaoCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoEmpresa = (Button) findViewById(R.id.empresaId);
     }
 }
