3968dcea7a4a5eb6af007147dc5e52c3ea758ddd
Lenon Tolfo <le.tolfo@yahoo.com.br>
2017-08-10 19:01:58 -0300

    Commit diario

--- AndroidStudioProjects/AtmEmpresaConsultoria/app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
+++ AndroidStudioProjects/AtmEmpresaConsultoria/app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
@@ -1,12 +1,15 @@
 package atmempresaconsultoria.cursoandroid.com.atmempresaconsultoria;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
-    private Button botaoEmpresa;
+    private ImageView botaoEmpresa;
     private Button botaoContato;
     private Button botaoServico;
     private Button botaoCliente;
@@ -16,6 +19,13 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        botaoEmpresa = (Button) findViewById(R.id.empresaId);
+        botaoEmpresa = (ImageView) findViewById(R.id.empresaId);
+
+        botaoEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this,EmpresaActivity.class));
+            }
+        });
     }
 }
